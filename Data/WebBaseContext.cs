2033c91215a6061016c290c1db4849e3a32ffe41
Michel Miertschink <chereu.m@gmail.com>
2024-07-03 17:41:23 -0300

    Pais controler with Views

---
+++ Data/WebBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebBase.Models;
+
+namespace WebBase.Data
+{
+    public class WebBaseContext : DbContext
+    {
+        public WebBaseContext (DbContextOptions<WebBaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebBase.Models.Pais> Pais { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebBase.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebBaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebBaseContext") ?? throw new InvalidOperationException("Connection string 'WebBaseContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
